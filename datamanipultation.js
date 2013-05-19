7d011d0802bb2448c5c20e2fc8d3288a7fcb4523
ShaneQful <shane_quigley@hotmail.com>
2013-05-19 22:36:07 +0100

    lint fixes

--- datamanipultation.js
+++ datamanipultation.js
@@ -1,9 +1,10 @@
 //Needs so error checking for unavailable locations
 function displayPathString(workout) {
+    'use strict';
     var url = 'https://maps.googleapis.com/maps/api/staticmap?path=color:0x0000ff|weight:5|';
-    url += workout.location.lat+','+workout.location.long;
+    url += workout.location.lat + ',' + workout.location.long;
     $(workout.intervals).each(function () {
-        url += "|"+this.location.lat+','+this.location.long;
+        url += "|" + this.location.lat + ',' + this.location.long;
     });
     url += '&size=512x512&sensor=true';
     url = encodeURI(url);
